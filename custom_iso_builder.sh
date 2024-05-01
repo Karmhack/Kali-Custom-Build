9e1ab17a06c5f85f4bd0921a0d952d7bbab7236b
Karmhack <78755695+Karmhack@users.noreply.github.com>
2024-05-01 13:32:21 +0200

    Update custom_iso_builder.sh

--- custom_iso_builder.sh
+++ custom_iso_builder.sh
@@ -4,6 +4,6 @@ sudo apt update -y && sudo apt upgrade -y
 sudo apt install -y git live-build simple-cdd cdebootstrap curl
 git clone https://gitlab.com/Karmhack/kali-custom-build.git
 cd kali-custom-build/kali-config/variant-xfce/hooks/live
-sudo chmod +x AD_tools.chroot	codium.chroot	enum.chroot	firefox_Addons.chroot	postman.chroot
+sudo chmod +x AD_tools.chroot codium.chroot enum.chroot firefox_Addons.chroot postman.chroot
 cd ../../../../
 ./build.sh --variant xfce --live -v
