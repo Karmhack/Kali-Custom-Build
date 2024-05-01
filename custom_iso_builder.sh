12128823abfbf54875afa1831a8183b8760ba5fd
Karmhack <78755695+Karmhack@users.noreply.github.com>
2024-05-01 21:37:55 +0200

    Delete custom_iso_builder.sh

--- custom_iso_builder.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sudo apt update -y && sudo apt upgrade -y
-sudo apt install -y git live-build simple-cdd cdebootstrap curl
-git clone https://gitlab.com/Karmhack/kali-custom-build.git
-cd kali-custom-build/
-./build.sh --variant xfce --live -v
