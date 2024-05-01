f7ae357397a70e189b6bbf77a071b721b7a27d16
Karmhack <78755695+Karmhack@users.noreply.github.com>
2024-05-01 12:27:37 +0200

    Create custom_iso_builder.sh

---
+++ custom_iso_builder.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install -y git live-build simple-cdd cdebootstrap curl
+git lab https://gitlab.com/Karmhack/kali-custom-build.git
+cd kali-custom-build/live-build-config
+./build.sh --variant xfce --live -v
